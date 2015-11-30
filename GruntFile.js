cbfe96a7cb273e562f737207204363de83785411
stenebenau <sten.ebenau@duplicatecheck.com>
2015-11-30 15:31:27 +0100

    Added Concat grunt

--- GruntFile.js
+++ GruntFile.js
@@ -3,6 +3,16 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg : grunt.file.readJSON('package.json'),
+        concat : {
+            options: {
+                banner : '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                
+            },
+            build : {
+                src : ['src/<%= pkg.name %>.js', 'src/plauti-multi-select.js', 'src/plauti-pick-list.js', 'src/plauti-time-picker.js', 'src/plauti-date-picker.js', 'src/plauti-date-time-picker.js', 'src/plauti-typeahead.js'],
+                dest : 'build/<%= pkg.name %>.js'
+            }  
+        },
         uglify : {
             options : {
                 banner : '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
@@ -15,7 +25,7 @@ module.exports = function(grunt) {
             },
             build : {
                 src : ['src/<%= pkg.name %>.js', 'src/plauti-multi-select.js', 'src/plauti-pick-list.js', 'src/plauti-time-picker.js', 'src/plauti-date-picker.js', 'src/plauti-date-time-picker.js', 'src/plauti-typeahead.js'],
-                dest : 'build/<%= pkg.name %>.<%= pkg.version %>.min.js'
+                dest : 'build/<%= pkg.name %>.min.js'
             }
 
         }
@@ -23,8 +33,10 @@ module.exports = function(grunt) {
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['uglify', 'concat']);
+    
 
 }; 
